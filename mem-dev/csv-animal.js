99ba4d975f8083c1ef21a56f005370405c64127b
philip000ict <philip000ict@gmail.com>
2024-11-18 16:32:14 +1100

    Mem-Dev add Animals csv

---
+++ mem-dev/csv-animal.js
@@ -0,0 +1,6 @@
+
+let csvtiles = ["img/animal/batsm.png","bat","img/animal/bunnysm.png","bunny","img/animal/catsm.png","cat","img/animal/clownfishsm.png","clownfish","img/animal/dogsm.png","dog","\
+  img/animal/dolphinsm.png","dolphin","img/animal/eaglesm.png","eagle","img/animal/emusm.png","emu","img/animal/fantailsm.png","fantail","img/animal/goldfishsm.png","goldfish","\
+  img/animal/horsesm.png","horse","img/animal/joeysm.png","joey","img/animal/kangaroosm.png","kangaroo","img/animal/kangaroosm.png","kangaroo","img/animal/kittensm.png","kitten","\
+  img/animal/owlsm.png","owl","img/animal/pelicansm.png","pelican","img/animal/penguinsm.png","penguin","img/animal/ponysm.png","pony","img/animal/puppysm.png","puppy","\
+  img/animal/rabbitsm.png","rabbit","img/animal/ratsm.png","rat","img/animal/seagullsm.png","seagull","img/animal/sealsm.png","seal","img/animal/whalesm.png","whale"];
